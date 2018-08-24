21e3440c441603b8ac97f66212a2bd194f79cb1c
ox223252 <23252@laposte.net>
2018-08-24 19:36:21 +0200

    Add config functions and write headers  - uartInit ( )  - uartSetReadTimeout ( )
    warning no test done
    
    Changes to be committed:
    	modified:   uart.c
    	modified:   uart.h

---
+++ uart.c
@@ -0,0 +1,219 @@
+#include "uart.h"
+
+#ifdef __linux__
+#include <termios.h>
+// ressources :
+// http://manpagesfr.free.fr/man/man3/termios.3.html
+#else
+// ressources :
+// https://msdn.microsoft.com/en-us/library/ff802693.aspx
+// https://code.msdn.microsoft.com/windowsdesktop/Serial-Port-Sample-e8accf30/sourcecode?fileId=67164&pathId=1394200469
+#endif
+
+#ifdef __linux__
+int uartInit ( uartHandler bus, uint32_t speed, UART_FLAGS flags )
+{
+	uint16_t i = 0;
+	struct termios uart;
+
+	// get old mask
+	if ( tcgetattr ( bus, &uart ) )
+	{
+		return ( __LINE__ );
+	}
+
+	// set speed
+	cfsetospeed ( &uart, speed );
+	cfsetispeed ( &uart, speed );
+
+	// set config
+	for ( i = 1; i != 0; i <<= 1 )
+	{
+		switch ( flags )
+		{
+			case UART_DATA_5:
+			{
+				uart.c_cflag &= ~CSIZE;
+				uart.c_cflag |= CS5;
+				break;
+			}
+			case UART_DATA_6:
+			{
+				uart.c_cflag &= ~CSIZE;
+				uart.c_cflag |= CS6;
+				break;
+			}
+			case UART_DATA_7:
+			{
+				uart.c_cflag &= ~CSIZE;
+				uart.c_cflag |= CS7;
+				break;
+			}
+			case UART_DATA_8:
+			{
+				uart.c_cflag &= ~CSIZE;
+				uart.c_cflag |= CS8;
+				break;
+			}
+			case UART_PARITY_N:
+			{
+				uart.c_cflag &= ~PARENB;
+				break;
+			}
+			case UART_PARITY_O:
+			{
+				uart.c_cflag |= PARENB;
+				uart.c_cflag |= PARODD;
+				break;
+			}
+			case UART_PARITY_E:
+			{
+				uart.c_cflag |= PARENB;
+				uart.c_cflag &= ~PARODD;
+				break;
+			}
+			case UART_STOP_1:
+			{
+				uart.c_cflag &= ~CSTOPB;
+				break;
+			}
+			case UART_STOP_2:
+			{
+				uart.c_cflag |= CSTOPB;
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	// clean rest of struct
+	uart.c_iflag = 0;
+	uart.c_lflag = 0;
+	uart.c_oflag = 0;
+
+	return ( tcsetattr ( bus, TCSANOW, &uart ) );
+}
+
+int uartSetReadTimeout ( uartHandler bus, uint8_t time, uint8_t min )
+{
+	static struct termios uart;
+
+	tcgetattr ( bus, &uart );
+	
+	uart.c_cc[ VMIN ] = min;
+	uart.c_cc[ VTIME ] = time;
+
+	return ( tcsetattr( bus, TCSANOW, &uart ) );
+}
+#else
+int uartInit ( uartHandler bus, uint32_t speed, UART_FLAGS flags )
+{
+	uint16_t i = 0;
+	DCB dcb;
+
+	FillMemory ( &dcb, sizeof ( dcb ), 0 );
+
+	// get old mask
+	if ( GetCommState ( bus, &dcb ) == 0 )
+	{
+		return ( __LINE__ );
+	}
+
+	// set speed
+	dcb.BaudRate = speed ;
+
+	// set config
+	for ( i = 1; i != 0; i <<= 1 )
+	{
+		switch ( flags )
+		{
+			case UART_DATA_5:
+			{
+				dcb.ByteSize = 5;
+				break;
+			}
+			case UART_DATA_6:
+			{
+				dcb.ByteSize = 6;
+				break;
+			}
+			case UART_DATA_7:
+			{
+				dcb.ByteSize = 7;
+				break;
+			}
+			case UART_DATA_8:
+			{
+				dcb.ByteSize = 8;
+				break;
+			}
+			case UART_PARITY_N:
+			{
+				dcb.fParity = 0;
+				dcb.Parity = NOPARITY;
+				break;
+			}
+			case UART_PARITY_O:
+			{
+				dcb.fParity = 1;
+				dcb.Parity = ODDPARITY;
+				break;
+			}
+			case UART_PARITY_E:
+			{
+				dcb.fParity = 1;
+				dcb.Parity = EVENPARITY;
+				break;
+			}
+			case UART_STOP_1:
+			{
+				dcb.StopBits = ONESTOPBIT;
+				break;
+			}
+			case UART_STOP_2:
+			{
+				dcb.StopBits = TWOSTOPBITS;
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	// windows require that was true
+	dcb.fBinary = 1;
+	
+	// clean rest of struct
+	dcb.fDtrControl = DTR_CONTROL_DISABLE;
+	dcb.fOutxCtsFlow = 0;
+	dcb.fOutxDsrFlow = 0;
+	dcb.fDsrSensitivity = 0;
+	dcb.fOutX = 0;
+	dcb.fInX = 0;
+	dcb.fNull = 0;
+	dcb.fRtsControl = RTS_CONTROL_DISABLE;
+
+	// Set new state.
+	return ( SetCommState ( bus, &dcb ) == 0 );
+}
+
+int uartSetReadTimeout ( uartHandler bus, uint8_t time, uint8_t min )
+{
+	COMMTIMEOUTS timeouts;
+
+	timeouts.ReadIntervalTimeout = time * 100; 
+	timeouts.ReadTotalTimeoutMultiplier = ( time )? min: 0;
+	timeouts.ReadTotalTimeoutConstant = ( time )? 1: 0;
+	timeouts.WriteTotalTimeoutMultiplier = 0;
+	timeouts.WriteTotalTimeoutConstant = 0;
+
+	return ( SetCommTimeouts ( bus, &timeouts ) == 0 );
+}
+
+
+#endif
--- uart.h
+++ uart.h
@@ -1,20 +1,104 @@
 #ifndef __UART_H__
 #define __UART_H__
 
+#include <stdint.h>
+
 #ifdef __linux__
 #include <unistd.h>
-typedef int uartHandler;
 #else
 #include <windows.h>
+#endif
+
+typedef enum
+{
+	UART_DATA_5 = 0x01,
+	UART_DATA_6 = 0x02,
+	UART_DATA_7 = 0x04,
+	UART_DATA_8 = 0x08,
+	UART_PARITY_N = 0x10, // no parity
+	UART_PARITY_O = 0x20, // parity odd
+	UART_PARITY_E = 0x40, // parity even
+	UART_STOP_1 = 0x80,
+	UART_STOP_2 = 0x100
+}
+UART_FLAGS;
+
+/// \typedef uartHandler
+/// \brief var type to store uart handler
+#ifdef __linux__
+typedef int uartHandler;
+#else
 typedef HANDLE uartHandler;
 #endif
 
-uartHandler uartOpen ( const char * const busName , int flags );
+////////////////////////////////////////////////////////////////////////////////
+/// \fn uartHandler uartOpen ( const char * const busName, int flags );
+/// \param[ in ] busName : string what contain bus name
+///     Linux: "/dev/ttyACMx"
+///     Windows : "COMx"
+/// \param[ in ] flags : 
+/// \brief this function open uart
+/// \retrun uart handler or error code need to be tested by uartValide ( )
+////////////////////////////////////////////////////////////////////////////////
+uartHandler uartOpen ( const char * const busName, int flags );
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int uartInit ( uartHandler bus, uint32_t speed, UART_FLAGS flags );
+/// \param[ in ] bus : handler returned by uartOpen ( )
+/// \param[ in ] speed : baudrate available
+///     50, 75, 110,  134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600
+///     19200, 38400, 57600, 115200, 230400
+/// \param[ in ] flags : flag to config, if an element is not set 
+///     it will not change
+/// \brief this will set uart port to the defined config
+/// \retrun if 0 then OK else error
+////////////////////////////////////////////////////////////////////////////////
+int uartInit ( uartHandler bus, uint32_t speed, UART_FLAGS flags );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int uartSetReadTimeout ( uartHandler bus, uint8_t time, uint8_t min );
+/// \param[ in ] bus : handler returned by uartOpen ( )
+/// \param[ in ] time : max time between two bytes
+/// \param[ in ] min : nb min of bytes read before return
+/// \biref set the number min for a read and the max time bewteen two byte.
+///     one the nb min of byte read if the if the timeout is reach the function
+///     return
+/// \return if 0 OK else error
+/// \bug i'm not sure this function fully works on Windows
+////////////////////////////////////////////////////////////////////////////////
+int uartSetReadTimeout ( uartHandler bus, uint8_t time, uint8_t min );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void uartWrite ( uartHandler bus, uint8_t * buf, size_t size );
+/// \param[ in ] bus : handler returned by uartOpen ( )
+/// \param[ in ] buf : buffer of data
+/// \param[ in ] size : buffer max size
+/// \biref write data on uart from a buffer
+////////////////////////////////////////////////////////////////////////////////
 void uartWrite ( uartHandler bus, uint8_t * buf, size_t size );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void uartRead ( uartHandler bus, uint8_t * buf, size_t size );
+/// \param[ in ] bus : handler returned by uartOpen ( )
+/// \param[ in ] buf : buffer to store read data
+/// \param[ in ] size : buffer max size
+/// \biref read data from uart to a buffer
+////////////////////////////////////////////////////////////////////////////////
 void uartRead ( uartHandler bus, uint8_t * buf, size_t size );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void uartClose ( uartHandler bus );
+/// \brief close uart handler
+////////////////////////////////////////////////////////////////////////////////
 void uartClose ( uartHandler bus );
-bool uartValide ( uartHandler bus );
+
+////////////////////////////////////////////////////////////////////////////////
+/// \fn int uartValide ( uartHandler bus );
+/// \param[ in ] bus : handler returned by uartOpen ( )
+/// \biref test if handler is valide
+/// \return if 0 then handler is valide else it's not valide
+////////////////////////////////////////////////////////////////////////////////
+int uartValide ( uartHandler bus );
 
 #ifdef __linux__
 #define uartWrite(bus,buf,size) write(bus,buf,size) 
